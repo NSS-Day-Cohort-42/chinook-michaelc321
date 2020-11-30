77e8da641ae5f05ee2316acb86ad0d1656d83092
michaelc321 <michaelc321@yahoo.com>
2020-11-30 10:30:45 -0600

    7

---
+++ chinook.sql
@@ -0,0 +1,25 @@
+SELECT FirstName, CustomerId, Country 
+FROM Customer 
+WHERE Country != "USA";
+
+SELECT * 
+FROM Customer 
+WHERE Country = "Brazil";
+
+SELECT Customer.FirstName, Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry
+FROM Invoice 
+INNER JOIN Customer ON Invoice.CustomerId=Customer.CustomerId
+WHERE BillingCountry = "Brazil";
+
+SELECT * FROM Employee WHERE title LIKE "Sales%";
+
+SELECT BillingCountry FROM Invoice;
+
+SELECT *, Employee.FirstName, Employee.LastName 
+FROM Invoice
+INNER JOIN Employee ON Employee.EmployeeId=Invoice.InvoiceId
+WHERE Employee.Title LIKE "Sales%"
+
+SELECT Invoice.Total, Customer.FirstName, Customer.LastName, Invoice.BillingCountry
+FROM Invoice
+INNER JOIN Customer ON Invoice.CustomerId=Customer.CustomerId;
